18df74fbbbf50d29d751de15d2dd5fd65410d9c0
Mario Colque <colquemario@gmail.com>
2015-10-24 08:14:47 -0300

    fixed css

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -4,36 +4,6 @@ angular.module('enbitcoins.controllers')
   .controller('HomeCtrl', ['$rootScope', '$scope', '$location', 'notifications', function($rootScope, $scope, $location, notifications) {
 
     $scope.init = function() {
-      $scope.step = 0;
-      var status = $location.search().status;
-
-      switch (status) {
-      case 'waitingForBitcoins':
-        $scope.step = 0;
-        break;
-
-      case 'waitingForConfirmations':
-        $scope.step = 1;
-        break;
-
-      case 'waitingForMoreBitcoins':
-        $scope.step = 1;
-        break;
-
-      case 'waitingForProvision':
-        $scope.step = 2;
-        break;
-
-      case 'provisioned':
-        $scope.step = 3;
-        break;
-      }
-
-      $scope.tx = {
-        status: status
-      };
     };
-
-
 
   }]);
